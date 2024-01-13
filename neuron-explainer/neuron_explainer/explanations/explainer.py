06e93e279d0b2d8607183d5231bdc91b937077c9
Johnny Lin <hijohnnylin@gmail.com>
2024-01-12 17:41:12 -0800

    Add gpt4-turbo to explainer models

--- neuron-explainer/neuron_explainer/explanations/explainer.py
+++ neuron-explainer/neuron_explainer/explanations/explainer.py
@@ -62,7 +62,7 @@ class ContextSize(int, Enum):
         raise ValueError(f"{i} is not a valid ContextSize")
 
 
-HARMONY_V4_MODELS = ["gpt-3.5-turbo", "gpt-4"]
+HARMONY_V4_MODELS = ["gpt-3.5-turbo", "gpt-4", "gpt-4-1106-preview"]
 
 
 class NeuronExplainer(ABC):
